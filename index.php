6cda8784fece3873ecbae2fc2fcae87c08eed1a5
Esteban Caccin <80750747+Bigmasters@users.noreply.github.com>
2021-06-11 16:51:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title class="titletyped">Boar's Head Cafe</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/theme/dist/img/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/theme/dist/img/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/theme/dist/img/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/theme/dist/img/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/theme/dist/img/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/theme/dist/img/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/theme/dist/img/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/theme/dist/img/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/theme/dist/img/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/theme/dist/img/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/theme/dist/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/theme/dist/img/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/theme/dist/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/theme/dist/img/favicons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/theme/dist/img/favicons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/theme/dist/vendor/bootstrap/css/bootstrap.min.css">
+
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="/theme/dist/vendor/animate/animate.css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="/theme/dist/fonts/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="/theme/dist/fonts/gotham/fonts.css">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="/theme/dist/css/main.min.css">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg fixed-top m-0 p-0">
+  <div class="container-fluid m-0 p-0" id="navContainer">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center m-0 p-0" id="navbarNav">
+  
+            <ul class="nav">
+              <li class="nav-item active">
+                <a href="#about" class="nav-link">ABOUT</a>
+              </li>
+              <li class="nav-item">
+                <a href="#menu" class="nav-link">MENU</a>
+              </li>
+              <li class="nav-item">
+                <a href="#locations" class="nav-link">LOCATIONS</a>
+              </li>                           
+            </ul>  
+            <img src="theme/dist/img/ordernow.png" height="80px" class="d-inline-block align-text-top" alt="fondoimg" id="logoimg">            
+          </div>
+        </div>
+      </nav>
+    <header id="home-section">
+        <div class="dark-overlay">
+         <div class="header-container">
+          <div class="row justify-content-center">
+          <img class="img-fluid main-img-logo" src="./theme/dist/img/boarslogo.png" alt="">
+           <div class="col-6 main-col">  
+             <div class="title-img"><img class="img-fluid main-img-text-logo" src="./theme/dist/img/boarsheadimg.png" alt=""></div>
+             <h3 class="text-center mainh3">Welcome to a world of </h3>
+             <h2 class="text-center mainh2">fine tastes & unlimited possibilities. </h2>
+             <p class="text-center mainp"> A café unlike any other, where the exceptional flavors of Boar’s Head® products are crafted with the culinary skill of our very own chefs. </p>
+           </div>
+          </div>
+          <div class="mainp main-location justify-content-center"><p class="main-locationp text-uppercase pt-4">view locations</p></div>
+        </div>                               
+      </div>
+    </header>
+<main>
+  <section class="section-compromise">
+    <div class="container">
+     <div class="row justify-content-center pt-5">
+     <div class="col-6">
+      <p class="compromisep text-center">______</p>
+      <p class="compromisep2 text-center">Rule No. 09</p>
+      <h1 class="compromiseh1">Compromise Elsewhere®</h1>
+      <p class="compromisep text-center">______</p>
+      <p class="compromisep3">For over 115 years, we haven’t wavered from our standards. And we never will.</p>
+      <p class="compromisep4">Boar’s Head CaféSM is the culmination of our artisanship, quality, and flavor in a modern deli-inspired experience. A place all our own, just for you, without compromise.</p>
+     </div>
+     </div>
+     <div class="row justify-cotent-center py-5">
+       <div class="col-sm-12 col-lg-6 p-0">
+         <img class="img-fluid compromise-img" src="./theme/dist/img/bg/background1.png" alt="">
+       </div>
+       <div class="col-sm-12 col-lg-6 p-0">
+       <img class="img-fluid compromise-img" src="./theme/dist/img/bg/background2.png" alt="">
+       </div>
+       <div class="row justify-content-center pt-5">
+     <div class="col-6">
+      <p class="compromisep2 text-center">Brooklyn Born,</p>
+      <h1 class="compromiseh1">meant for everyone™</h1>
+      <p class="compromisep text-center">______</p>
+      <p class="compromisep4">We are proud to offer a variety of unique culinary experiences. Our sit-down restaurant hearkens back to the delis of yesteryear, our food truck delivers excellence around the block, and new destinations are popping up fast. Come see us. We’ll make it worth your while.</p>
+      </div>
+      <div class="row pt-4">
+      <div class="col-6">
+      <p class="text-end compromiseny">New York</p>
+      </div>
+      <div class="col-6">
+      <p class="text-right compromisech">Chicago</p>
+      </div>
+      <div class="row justify-cotent-center py-5">
+       <div class="col-sm-12 col-lg-4 p-0">
+         <img class="img-fluid compromise-img" src="./theme/dist/img/chicago1.png" alt="">
+       </div>
+       <div class="col-sm-12 col-lg-4 p-0">
+       <img class="img-fluid compromise-img" src="./theme/dist/img/chicago2.png" alt="">
+       </div>
+       <div class="col-sm-12 col-lg-4 p-0">
+       <img class="img-fluid compromise-img" src="./theme/dist/img/chicago3.png" alt="">
+       </div>
+     </div>
+     </div>
+     </div>
+    </div>
+  </section>
+</main>
+<footer>
+  <div class="container">
+    <div class="row py-5">
+  <div class="col-sm-12 col-lg-6 footer-col">
+<img class="logoboarshead" src="./theme/dist/img/logoboarshead.png" alt="">
+  </div>
+  <div class="col-sm-12 col-lg-6 text-end footer-col22">
+    <p class="familyp">A family owned business since 1905</p>
+   </div>
+   </div>
+
+  <div class="row pb-5">
+  <div class="col-sm-12 col-lg-6 footer-col1">
+  <div class="row footer-row1">
+  <div class="col-sm-12 col-lg-6 footer-col"> 
+    <p class="footerp">ABOUT</p>
+    <p class="footerp">MENU</p>
+    <p class="footerp">LOCATIONS</p>
+    <p class="footerp">ORDER NOW</p>
+</div>
+
+  <div class="col-sm-12 col-lg-6 footer-col1">
+    <p class="footerep m-4">NEW YORK</p>
+    <p class="text-center"><img src="./theme/dist/img/nylogo.png"  class="footer-img" alt=""></p>
+    <p class="text-center footerep2">787 Lexington Ave, New York, NY 10065</p>
+    <p class="text-center footerep2">—</p>
+    <div class="text-center footerpicon"> <img class="img-fluid footer-img-icon" src="./theme/dist/img/ig-icon.png" alt=""></div>
+</div>
+  </div>
+   </div>
+
+  <div class="col-sm-12 col-lg-6 footer-col1">
+  <div class="row footer-row2">
+    
+  <div class="col-xs-4 col-lg-4 footer-col"> 
+  <p class="footerep m-4">CHICAGO</p>
+    <p class="text-center"><img src="./theme/dist/img/ny2logo.png"  class="footer-img" alt=""></p>
+    <p class="text-center footerep2 m-0">210 W. Lake St. Chicago, IL 60606(312) 759-2233</p>
+<div class="text-center footerpicon"> <img class="img-fluid footer-img-icon3" src="./theme/dist/img/igfc-icon.png" alt=""></div>
+  </div>
+  <div class="col-xs-4 col-lg-4 footer-col1"> <p class="footerep2 m-4">.</p>
+    <p class="text-center"><img src="./theme/dist/img/ch2logo.png"  class="footer-img" alt=""></p>
+    <p class="text-center footerep2">125 S. Wacker Dr. Chicago, IL 60606 <br> 
+—</p>
+<div class="text-center footerpicon"> <img class="img-fluid footer-img-icon2" src="./theme/dist/img/ig-icon.png" alt=""></div></div>
+  
+
+<div class="col-xs-4 col-lg-4 footer-col1"> <p class="footerep2 m-4">.</p>
+    <p class="text-center"><img src="./theme/dist/img/nylogo.png"  class="footer-img" alt=""></p>
+    <p class="text-center footerep2">Follow us to see where our truck is today.
+—</p>
+<div class="text-center footerpicon"> <img class="img-fluid footer-img-icon2" src="./theme/dist/img/igfc-icon.png" alt=""></div></div></div>
+  </div>
+  </div>
+</footer>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/theme/dist/vendor/jquery/jquery.js"></script>
+<script src="/theme/dist/vendor/jquery/jquery-migrate.js"></script>
+
+<script src="/theme/dist/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="/theme/dist/js/main.min.js"></script>
+
+</body>
+</html>
